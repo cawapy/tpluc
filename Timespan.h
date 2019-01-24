ec2dc8da47f745eb353a2772ae5001d3f69c8ee8
Cawapy <31598347+cawapy@users.noreply.github.com>
2019-01-24 20:28:12 +0100

    Adding TimespanZero and IsZero constant to all timespans

--- Timespan.h
+++ Timespan.h
@@ -2,7 +2,22 @@
 
 namespace tpluc
 {
-    template<uint32_t ms> struct TimespanMs { static void Wait() { _delay_ms(ms); } };
-    template<uint32_t us> struct TimespanUs { static void Wait() { _delay_us(us); } };
+    template<uint32_t ms> struct TimespanMs
+    {
+        static const bool IsZero = ms == 0;
+        static void Wait() { _delay_ms(ms); }
+    };
+
+    template<uint32_t us> struct TimespanUs
+    {
+        static const bool IsZero = us == 0;
+        static void Wait() { _delay_us(us); }
+    };
+
+    struct TimespanZero
+    {
+        static const bool IsZero = true;
+        static void Wait() { }
+    };
 }
 
